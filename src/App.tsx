cf6defa3a7ac02fc2c5cebd5707033e68a6742ad
wangenius <wangenius@qq.com>
2025-03-01 18:13:47 +0800

    新版本：添加了生产环境的devtool

--- src/App.tsx
+++ src/App.tsx
@@ -14,42 +14,45 @@ import { WorkflowEditor } from "./workflow/WorkflowEditor";
 
 /* 主应用,提供路由 */
 function App() {
-    const theme = SettingsManager.use(selector => selector.theme);
-    const font = SettingsManager.use(selector => selector.font);
-    useEffect(() => {
-        const handleKeyDown = (e: KeyboardEvent) => {
-            if (e.key === "Alt") {
-                e.preventDefault();
-            }
-            if (e.key.toLowerCase() === "r" && e.ctrlKey) {
-                e.preventDefault();
-            }
-        };
-        window.addEventListener("keydown", handleKeyDown);
-        return () => {
-            window.removeEventListener("keydown", handleKeyDown);
-        };
-    }, []);
-    return (
-        <div data-theme={theme.name} data-font={font.name} id="app" className="h-full w-full bg-background">
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<MainView />} />
-                    <Route path="/settings" element={<SettingsPage />} />
-                    <Route path="/model-edit" element={<ModelEditor />} />
-                    <Route path="/bot-edit" element={<BotEditor />} />
-                    <Route path="/plugin-edit" element={<PluginEditor />} />
-                    <Route path="/env-edit" element={<EnvEditor />} />
-                    <Route path="/history" element={<HistoryPage />} />
-                    <Route path="/knowledge-creator" element={<KnowledgeCreator />} />
-                    <Route path="/knowledge-preview" element={<KnowledgePreview />} />
-                    <Route path="/workflow-edit" element={<WorkflowEditor />} />
-                </Routes>
-            </BrowserRouter>
-        </div>
-
-
-    );
+  const theme = SettingsManager.use((selector) => selector.theme);
+  const font = SettingsManager.use((selector) => selector.font);
+  useEffect(() => {
+    const handleKeyDown = (e: KeyboardEvent) => {
+      if (e.key === "Alt") {
+        e.preventDefault();
+      }
+      if (e.key.toLowerCase() === "r" && e.ctrlKey) {
+        e.preventDefault();
+      }
+    };
+    window.addEventListener("keydown", handleKeyDown);
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
+  return (
+    <div
+      data-theme={theme.name}
+      data-font={font.name}
+      id="app"
+      className="h-full w-full bg-background"
+    >
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainView />} />
+          <Route path="/settings" element={<SettingsPage />} />
+          <Route path="/model-edit" element={<ModelEditor />} />
+          <Route path="/bot-edit" element={<BotEditor />} />
+          <Route path="/plugin-edit" element={<PluginEditor />} />
+          <Route path="/env-edit" element={<EnvEditor />} />
+          <Route path="/history" element={<HistoryPage />} />
+          <Route path="/knowledge-creator" element={<KnowledgeCreator />} />
+          <Route path="/knowledge-preview" element={<KnowledgePreview />} />
+          <Route path="/workflow-edit" element={<WorkflowEditor />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
